d21dd7f37992c4ecc006eaa84650df8630fe99b4
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-10-15 23:54:44 +0200

    Add some tests + maven-compiler and other dependencies

--- labki/src/test/java/com/gmail/konradkalita/MatrixTest.java
+++ labki/src/test/java/com/gmail/konradkalita/MatrixTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixTest {
 
+    private Matrix matrix;
+
     @BeforeEach
     void setUp() {
         double[][] data = {
@@ -15,7 +17,7 @@ class MatrixTest {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        Matrix matrix = new Matrix(data);
+        matrix = new Matrix(data);
     }
 
     @AfterEach
@@ -24,26 +26,54 @@ class MatrixTest {
 
     @Test
     void asArray() {
+        double[][] expected = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        double[][] actual = matrix.asArray();
+        for (int i = 0; i < expected.length; i++) {
+            assertArrayEquals(expected[i], actual[i]);
+        }
+    }
+
+    @Test
+    void Matrix() {
+        int rows = 3, cols = 3;
+        assertEquals(matrix.getRows(), rows);
+        assertEquals(matrix.getCols(), cols);
     }
 
     @Test
     void getValue() {
+        assertEquals(matrix.getValue(1, 1), 1);
+        assertEquals(matrix.getValue(2, 2), 5);
+        assertEquals(matrix.getValue(3, 3), 9);
     }
 
     @Test
     void setValue() {
+        double newValue = 5.0;
+        matrix.setValue(1, 1, newValue);
+        assertEquals(matrix.getValue(1, 1), newValue);
     }
 
     @Test
     void setData() {
+        double[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        matrix.setData(expected);
+        assertArrayEquals(expected, matrix.getData());
     }
 
     @Test
-    void toString1() {
+    void testToString() {
+        String expected = "[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]";
+        assertEquals(expected, matrix.toString());
     }
 
     @Test
     void reshape() {
+
     }
 
     @Test
