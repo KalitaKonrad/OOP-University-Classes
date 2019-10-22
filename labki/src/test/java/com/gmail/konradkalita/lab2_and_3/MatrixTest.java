e7bbedf3fb8e5f1fbd6eb741364477a9f0a95b89
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-10-22 17:47:18 +0200

    Make all tests follow convention

--- labki/src/test/java/com/gmail/konradkalita/lab2_and_3/MatrixTest.java
+++ labki/src/test/java/com/gmail/konradkalita/lab2_and_3/MatrixTest.java
@@ -23,7 +23,7 @@ class MatrixTest
     }
 
     @Test
-    void asArray()
+    void Should_ReturnMatrixAsArray()
     {
         double[][] expected = {
                 {1, 2, 3},
@@ -38,7 +38,7 @@ class MatrixTest
     }
 
     @Test
-    void Matrix()
+    void Should_CreateMatrixWithCertainGivenDimensions()
     {
         int rows = 3, cols = 3;
         assertEquals(matrix.getRows(), rows);
@@ -46,7 +46,7 @@ class MatrixTest
     }
 
     @Test
-    void getValue()
+    void Should_GetMatrixValueFromCertainGivenPosition()
     {
         assertEquals(matrix.getValue(1, 1), 1);
         assertEquals(matrix.getValue(2, 2), 5);
@@ -54,7 +54,7 @@ class MatrixTest
     }
 
     @Test
-    void setValue()
+    void Should_SetMatrixValueAtCertainGivenPosition()
     {
         double newValue = 5.0;
         matrix.setValue(1, 1, newValue);
@@ -62,7 +62,7 @@ class MatrixTest
     }
 
     @Test
-    void setData()
+    void Should_SetMatrixData()
     {
         double[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         matrix.setData(expected);
@@ -70,14 +70,14 @@ class MatrixTest
     }
 
     @Test
-    void testToString()
+    void Should_ReturnMatrixConvertedToString()
     {
         String expected = "[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]";
         assertEquals(expected, matrix.toString());
     }
 
     @Test
-    void reshapeShouldThrowRuntimeException()
+    void Should_ThrowRuntimeException_When_WrongDimensionsAreProvided()
     {
         try
         {
@@ -89,14 +89,14 @@ class MatrixTest
     }
 
     @Test
-    void shape()
+    void Should_ReturnArrayWithTwoElements_Where_OneElementIsRowCountAndSecondOneIsColumnCount()
     {
         int[] expected = {3, 3};
         assertArrayEquals(expected, matrix.shape());
     }
 
     @Test
-    void addMatrixToMatrix()
+    void Should_ReturnMatrixAddedToItself()
     {
         Matrix expected = new Matrix(new double[][]{{2, 4, 6}, {8, 10, 12}, {14, 16, 18}});
         matrix = matrix.add(matrix);
@@ -107,7 +107,7 @@ class MatrixTest
     }
 
     @Test
-    void addTypeDoubleToEachMatrixElement()
+    void Should_AddFiveToEachMatrixElement()
     {
         Matrix expected = new Matrix(new double[][]{{6, 7, 8}, {9, 10, 11}, {12, 13, 14}});
         matrix.add(5);
@@ -116,7 +116,7 @@ class MatrixTest
     }
 
     @Test
-    void subTypeDoubleToEachMatrixElement()
+    void Should_SubtractFiveFromEachMatrixElement()
     {
         matrix.sub(5);
         double[] expectedData = new double[] {-4, -3, -2, -1, 0, 1, 2, 3, 4};
@@ -124,7 +124,7 @@ class MatrixTest
     }
 
     @Test
-    void subMatrixFromMatrix()
+    void Should_ReturnMatrixWithEveryElementSetToZero_When_SubtractingMatrixFromItself()
     {
         matrix = matrix.sub(matrix);
         double[] expectedData = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -132,7 +132,7 @@ class MatrixTest
     }
 
     @Test
-    void mulTypeDoubleToEachMatrixElement()
+    void Should_MultiplyEachElementInMatrixByTwo()
     {
         matrix.mul(2);
         double[] expectedData = new double[] {2, 4, 6, 8, 10, 12, 14, 16, 18};
@@ -140,7 +140,7 @@ class MatrixTest
     }
 
     @Test
-    void mulEachMatrixElementByCorrespondingMatrixElement()
+    void Should_ReturnEachMatrixElementMultipliedByItself()
     {
         double[] expectedData = new double[] {1, 4, 9, 16, 25, 36, 49, 64, 81};
         matrix = matrix.mul(matrix);
@@ -148,7 +148,7 @@ class MatrixTest
     }
 
     @Test
-    void div_divisionEachMatrixElementByTypeDouble()
+    void Should_ReturnMatrixWhereEachElementDividedByTwodiv()
     {
         double[] expected = matrix.getData();
         for (int i = 0; i < expected.length; i++)
@@ -156,11 +156,11 @@ class MatrixTest
             expected[i] /= 2;
         }
         matrix.div(2);
-        assertArrayEquals(matrix.getData(), expected);
+        assertArrayEquals(matrix.getData(), expected, 1e-7);
     }
 
     @Test
-    void Should__ThrowIllegalArgumentException_ByZeroDoubleInMatrix()
+    void Should_ThrowIllegalArgumentException_When_DividedByZeroInMatrix()
     {
         try
         {
@@ -169,11 +169,10 @@ class MatrixTest
         {
             assertEquals("Division by ZERO!", e.getMessage());
         }
-
-
     }
+
     @Test
-    void divisionByZeroElementInMatrixShouldThrowIllegalArgumentException()
+    void Should_ThrowIllegalArgumentException_When_DivisionByZeroInMatrixDivision()
     {
         Matrix test = new Matrix(new double[][] {{1, 1, 1}, {0, 0, 0}, {1, 1, 1}});
         try {
@@ -185,21 +184,21 @@ class MatrixTest
     }
 
     @Test
-    void dotMatrixByMatrixMultiplication()
+    void Should_ReturnMatrixMultiplyByItself()
     {
         double[] expected = new double[] {30, 36, 42, 66, 81, 96, 102, 126, 150};
         matrix = matrix.dot(matrix);
-        assertArrayEquals(expected, matrix.getData());
+        assertArrayEquals(expected, matrix.getData(), 1e-7);
     }
 
     @Test
-    void frobeniusMatrixSubstractedFromMatrixShouldHaveZeroFrobeniusNorm()
+    void Should_HaveZeroFrobeniusNorm_When_MatrixSubtractedFromItself()
     {
         assertEquals(0, matrix.sub(matrix).frobenius());
     }
 
     @Test
-    void frobeniusMatrixDividedByMatrixShouldHaveRowsMultipliedByColsFrobeniusNorm()
+    void Should_ReturnMultiplyRowsTimesColumns_When_MatrixDividedByItselfAndFrobeniusNormCounted()
     {
         assertEquals(3*3, matrix.div(matrix).frobenius());
     }
