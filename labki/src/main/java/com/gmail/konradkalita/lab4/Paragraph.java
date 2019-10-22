2ac975b05424b6b776d12902f077e44c368b4c86
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-10-22 15:02:08 +0200

    Fix paragraph class

--- labki/src/main/java/com/gmail/konradkalita/lab4/Paragraph.java
+++ labki/src/main/java/com/gmail/konradkalita/lab4/Paragraph.java
@@ -6,14 +6,21 @@ public class Paragraph implements HTMLElement
 {
     private String content;
 
+    public Paragraph(String content)
+    {
+        this.content = content;
+    }
+
     public Paragraph setContent(String content)
     {
         this.content = content;
+        return this;
     }
 
     @Override
     public PrintStream wirteHTML(PrintStream out)
     {
         out.printf("<p>%s</p>", content);
+        return out;
     }
 }
