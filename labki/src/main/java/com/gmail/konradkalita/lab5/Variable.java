797a0c411812e5d1979eb7cb3fb36786fbcff121
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-10-29 23:58:30 +0100

    Add variable class

---
+++ labki/src/main/java/com/gmail/konradkalita/lab5/Variable.java
@@ -0,0 +1,26 @@
+package com.gmail.konradkalita.lab5;
+
+public class Variable extends Node
+{
+    private String name;
+    private Double value;
+    Variable(String name)
+    {
+        this.name = name;
+    }
+    void setValue(double d)
+    {
+        value = d;
+    }
+
+    @Override
+    double evaluate() {
+        return getSign() * value;
+    }
+
+    @Override
+    public String toString() {
+        String sgn=getSign() < 0 ? "-" : "";
+        return sgn+name;
+    }
+}
