ed225e404a7897866f8f4294934de87bc1d236cb
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-12 16:44:08 +0100

    Add tryCatch in next(), fix parseHeader(), add sample_csv_files directory + files in there

--- labki/src/main/java/com/gmail/konradkalita/lab6/CSVReader.java
+++ labki/src/main/java/com/gmail/konradkalita/lab6/CSVReader.java
@@ -3,10 +3,7 @@ package com.gmail.konradkalita.lab6;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.Buffer;
 import java.util.*;
 import java.util.stream.IntStream;
@@ -18,9 +15,19 @@ public class CSVReader
     private BufferedReader reader;
     private String delimiter;
     private boolean hasHeader;
-    private List<String> columnLabels;
+    private List<String> columnLabels = new ArrayList<>();
     private Map<String,Integer> columnLabelsToInt = new HashMap<>();
     private String[] current;
+
+    public CSVReader(String filename) throws FileNotFoundException
+    {
+        this(filename, ",", false);
+    }
+
+    public CSVReader(String filename, String delimiter) throws FileNotFoundException
+    {
+        this(filename, delimiter, false);
+    }
 
     /**
      *
@@ -29,53 +36,44 @@ public class CSVReader
      * @param hasHeader - czy plik ma wiersz nagłówkowy
      */
 
-    public CSVReader(String filename) throws FileNotFoundException
-    {
-        this(filename, ",", false);
-    }
-
-    public CSVReader(String filename, String delimeter) throws FileNotFoundException
-    {
-        this(filename, delimeter, false);
-    }
-
     public CSVReader(String filename, String delimiter, boolean hasHeader) throws FileNotFoundException
     {
         this.reader = new BufferedReader(new FileReader(filename));
         this.delimiter = delimiter;
         this.hasHeader = hasHeader;
         if(hasHeader) {
-            try
-            {
-                parseHeader();
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+            parseHeader();
         }
     }
 
-    private void parseHeader() throws IOException
+    private void parseHeader()
     {
         if(!next()) {
             return;
         }
 
-        List<String> header = Arrays.asList(current);
-        IntStream.range(0, header.size())
-                .forEach(index -> columnLabelsToInt.put(header.get(index), index));
+        for (int i = 0; i < current.length; i++) {
+            columnLabelsToInt.put(current[i], i);
+            columnLabels.add(current[i]);  // ?
+        }
     }
 
-    public boolean next() throws IOException
+    public boolean next()
     {
-        String line = reader.readLine();
-        if (line == null) {
+        String line;
+        try
+        {
+            line = reader.readLine();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
             return false;
         }
 
         this.current = line.split(delimiter);
         return true;
     }
+
 
     public List<String> getColumnLabels() {
         return this.columnLabels;
--- labki/src/main/java/com/gmail/konradkalita/lab6/Main.java
+++ labki/src/main/java/com/gmail/konradkalita/lab6/Main.java
@@ -1,9 +1,28 @@
 package com.gmail.konradkalita.lab6;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Locale;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        CSVReader reader;
+        try
+        {
+            reader = new CSVReader("./src/main/java/com/gmail/konradkalita/lab6/sample_csv_files/with-header.csv",
+                    ",",true);
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            return;
+        }
+        while(reader.next()) {
+            for (int i = 0; i < reader.getCurrent().length; i++) {
+                System.out.println(reader.getCurrent()[i]);
+            }
+        }
     }
 }
