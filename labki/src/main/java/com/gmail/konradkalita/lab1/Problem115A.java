4c05c38c934f948e00be49be8bb13e74eb49bd92
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-13 23:08:55 +0100

    Add implementation of problem115A

--- labki/src/main/java/com/gmail/konradkalita/lab1/Problem115A.java
+++ labki/src/main/java/com/gmail/konradkalita/lab1/Problem115A.java
@@ -6,6 +6,34 @@ public class Problem115A
 {
     public static void main(String[] args)
     {
+        solution();
+    }
 
+    private static void solution() {
+        Scanner scanner = new Scanner(System.in);
+        int workersCount = scanner.nextInt();
+
+        int answer = 0;
+        int count = 0;
+
+        int[] workers = new int[workersCount + 9];
+
+        for (int i = 1; i < workersCount + 1; i++) {
+            workers[i] = scanner.nextInt();
+        }
+
+        for (int i = 1; i < workersCount + 1; i++) {
+            count = 0;
+            int worker = workers[i];
+
+            while (worker != -1) {
+                worker = workers[worker];
+                count++;
+            }
+
+            answer = Math.max(answer, count);
+        }
+
+        System.out.println(answer + 1);
     }
 }
