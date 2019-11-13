a5b9e644cbcd3472f7dffe298ea767e492036f2d
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-13 22:26:17 +0100

    Replace wrong encoded characters with polish replacement

---
+++ labki/src/test/java/com/gmail/konradkalita/lab6/CSVReaderTest.java
@@ -0,0 +1,62 @@
+package com.gmail.konradkalita.lab6;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVReaderTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void get()
+    {
+    }
+
+    @Test
+    void get1()
+    {
+    }
+
+    @Test
+    void getInt()
+    {
+    }
+
+    @Test
+    void getInt1()
+    {
+    }
+
+    @Test
+    void getLong()
+    {
+    }
+
+    @Test
+    void getLong1()
+    {
+    }
+
+    @Test
+    void getDouble()
+    {
+    }
+
+    @Test
+    void getDouble1()
+    {
+    }
+}
