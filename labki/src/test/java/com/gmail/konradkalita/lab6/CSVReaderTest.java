b645dbe4efa2d1e299fcb83f0082bd2c711c8137
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-19 00:44:49 +0100

    Add AssertJ dependency, add some unit tests

--- labki/src/test/java/com/gmail/konradkalita/lab6/CSVReaderTest.java
+++ labki/src/test/java/com/gmail/konradkalita/lab6/CSVReaderTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.StringReader;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CSVReaderTest
 {
@@ -41,14 +42,39 @@ class CSVReaderTest
     }
 
     @Test
-    void get()
+    void should_ParseHeader_When_GivenFileHasHeader()
     {
+        assertArrayEquals(new String[]{"id", "imię", "nazwisko", "ulica", "nrdomu", "nrmieszkania"},
+                with_header.getCurrent());
+
     }
 
     @Test
-    void get1()
+    void should_ThrowColumnNotFoundException_When_GivenColumnIndexIsTooLarge()
     {
-        
+        int columnIndex = 100; // There are only columns from zero to fifth (including)
+        assertThatExceptionOfType(ColumnNotFoundException.class)
+                .isThrownBy(() -> with_header.get(columnIndex))
+                .withMessage(String.format("Column with number: %d does not exist", columnIndex));
+    }
+
+    @Test
+    void should_ThrowColumnNotFoundException_When_GivenColumnIndexIsTooSmall()
+    {
+        int columnIndex = -1; // There are only columns from zero to fifth (including)
+        assertThatExceptionOfType(ColumnNotFoundException.class)
+                .isThrownBy(() -> with_header.get(columnIndex))
+                .withMessage(String.format("Column with number: %d does not exist", columnIndex));
+    }
+
+    @Test
+    void should_ThrowEmptyColumnValueException_When_GivenColumnIndexValueIsEmpty()
+    {
+        with_header.setCurrent(new String[]{"", "imię", "nazwisko", "ulica", "nrdomu", "nrmieszkania"}); // zeroth column is set empty
+        int columnIndex = 0; // index of empty column
+        assertThatExceptionOfType(EmptyColumnValueException.class)
+                .isThrownBy(() -> with_header.get(columnIndex))
+                .withMessage(String.format("Value on column: %d does not exist", columnIndex));
     }
 
     @Test
