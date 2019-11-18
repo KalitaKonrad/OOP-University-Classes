0d39ca685e0f5848588e694db886275c7f18d376
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-18 23:29:44 +0100

    Add Date and Time functions, make public all suitable functions

--- labki/src/main/java/com/gmail/konradkalita/lab6/CSVReader.java
+++ labki/src/main/java/com/gmail/konradkalita/lab6/CSVReader.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -23,9 +25,8 @@ public class CSVReader
 
     private static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
     private static final String DEFAULT_DATE_FORMAT = "dd.MM.yyyy";
-    private static final String REGEX_FOR_SPLITTING = "%s(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
     private static final String DEFAULT_DATE_AND_TIME_FORMAT = DEFAULT_TIME_FORMAT + " " + DEFAULT_DATE_FORMAT;
-
+    private static final String REGEX_FOR_SPLITTING = "%s(?=([^\"]*\"[^\"]*\")*[^\"]*$)"; //%s at beginning to insert provided delimiter there
 
     public CSVReader(String filename) throws IOException
     {
@@ -129,39 +130,83 @@ public class CSVReader
         return current[columnIndex].isEmpty();
     }
 
-    String get(int columnIndex) throws ColumnNotFoundException, EmptyColumnValueException {
+    public String get(int columnIndex) throws ColumnNotFoundException, EmptyColumnValueException {
         return isMissing(columnIndex) ? "" : current[columnIndex];
     }
 
-    String get(String columnLabel) throws ColumnNotFoundException, EmptyColumnValueException {
+    public String get(String columnLabel) throws ColumnNotFoundException, EmptyColumnValueException {
         return isMissing(columnLabel) ? "" : current[columnLabelsToInt.get(columnLabel)];
     }
 
-    int getInt(int columnIndex) throws EmptyColumnValueException, ColumnNotFoundException {
+    public int getInt(int columnIndex) throws EmptyColumnValueException, ColumnNotFoundException {
         return Integer.parseInt(get(columnIndex));
     }
 
-    int getInt(String columnLabel) throws EmptyColumnValueException, ColumnNotFoundException  {
+    public int getInt(String columnLabel) throws EmptyColumnValueException, ColumnNotFoundException  {
         return Integer.parseInt(get(columnLabelsToInt.get(columnLabel)));
     }
 
-    Long getLong(int columnIndex) throws EmptyColumnValueException, ColumnNotFoundException  {
+    public Long getLong(int columnIndex) throws EmptyColumnValueException, ColumnNotFoundException  {
         return Long.parseLong(get(columnIndex));
     }
 
-    Long getLong(String columnLabel) throws EmptyColumnValueException, ColumnNotFoundException  {
+    public Long getLong(String columnLabel) throws EmptyColumnValueException, ColumnNotFoundException  {
         return Long.parseLong(get(columnLabelsToInt.get(columnLabel)));
     }
 
-    Double getDouble(int columnIndex) throws EmptyColumnValueException, ColumnNotFoundException  {
+    public Double getDouble(int columnIndex) throws EmptyColumnValueException, ColumnNotFoundException  {
         return Double.parseDouble(get(columnIndex));
     }
 
-    Double getDouble(String columnLabel) throws EmptyColumnValueException, ColumnNotFoundException {
+   public Double getDouble(String columnLabel) throws EmptyColumnValueException, ColumnNotFoundException {
         return Double.parseDouble(get(columnLabelsToInt.get(columnLabel)));
     }
-/*
-    LocalTime getTime(int columnIndex, String format) {
-        return LocalTime.parse(get(columnIndex), DateTimeFormatter.ofPattern("HH:mm"));
-    }*/
+
+    public LocalTime getTime(int index) throws ColumnNotFoundException, EmptyColumnValueException {
+        return getTime(index, DEFAULT_TIME_FORMAT);
+    }
+
+    public LocalTime getTime(int index, String format) throws EmptyColumnValueException, ColumnNotFoundException {
+        return LocalTime.parse(get(index), DateTimeFormatter.ofPattern(format));
+    }
+
+    public LocalTime getTime(String column) throws ColumnNotFoundException, EmptyColumnValueException {
+        return getTime(column, DEFAULT_TIME_FORMAT);
+    }
+
+    public LocalTime getTime(String column, String format) throws ColumnNotFoundException, EmptyColumnValueException {
+        return LocalTime.parse(get(column), DateTimeFormatter.ofPattern(format));
+    }
+
+    public LocalDate getDate(int index) throws ColumnNotFoundException, EmptyColumnValueException {
+        return getDate(index, DEFAULT_DATE_FORMAT);
+    }
+
+    public LocalDate getDate(int index, String format) throws EmptyColumnValueException, ColumnNotFoundException {
+        return LocalDate.parse(get(index), DateTimeFormatter.ofPattern(format));
+    }
+
+    public LocalDate getDate(String column) throws ColumnNotFoundException, EmptyColumnValueException {
+        return getDate(column, DEFAULT_DATE_FORMAT);
+    }
+
+    public LocalDate getDate(String column, String format) throws ColumnNotFoundException, EmptyColumnValueException {
+        return LocalDate.parse(get(column), DateTimeFormatter.ofPattern(format));
+    }
+
+    public LocalDateTime getDateTime(int index) throws ColumnNotFoundException, EmptyColumnValueException {
+        return getDateTime(index, DEFAULT_DATE_AND_TIME_FORMAT);
+    }
+
+    public LocalDateTime getDateTime(int index, String format) throws EmptyColumnValueException, ColumnNotFoundException {
+        return LocalDateTime.parse(get(index), DateTimeFormatter.ofPattern(format));
+    }
+
+    public LocalDateTime getDateTime(String column) throws EmptyColumnValueException, ColumnNotFoundException {
+        return getDateTime(column, DEFAULT_DATE_AND_TIME_FORMAT);
+    }
+
+    public LocalDateTime getDateTime(String column, String format) throws ColumnNotFoundException, EmptyColumnValueException {
+        return LocalDateTime.parse(get(column), DateTimeFormatter.ofPattern(format));
+    }
 }
