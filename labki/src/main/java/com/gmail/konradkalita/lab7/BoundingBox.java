01b9fe7bc600d1ed25d71be871fd21eee247af9e
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-26 15:15:17 +0100

    Add more implementation

--- labki/src/main/java/com/gmail/konradkalita/lab7/BoundingBox.java
+++ labki/src/main/java/com/gmail/konradkalita/lab7/BoundingBox.java
@@ -1,6 +1,7 @@
 package com.gmail.konradkalita.lab7;
 
 import com.gmail.konradkalita.lab2_and_3.Matrix;
+import jdk.internal.util.xml.impl.Pair;
 import lombok.ToString;
 
 @ToString
@@ -11,15 +12,26 @@ public class BoundingBox {
     double yMax;
 
     void addPoint(double x, double y) {
-
+        if(isEmpty()) {
+            xMin = xMax = x;
+            yMin = yMax = y;
+            return;
+        }
+        else if(contains(x,y)) {
+            return;
+        }
+        xMin = Math.min(xMin, x);
+        xMax = Math.max(xMax, x);
+        yMin = Math.min(yMin, y);
+        yMax = Math.max(yMax, y);
     }
 
     boolean contains(double x, double y){
-        return x >= xMin && x <= xMax && y >= yMin && y <= yMax;
+        return !isEmpty() && x >= xMin && x <= xMax && y >= yMin && y <= yMax;
     }
 
     boolean contains(BoundingBox box) {
-        return contains(box.xMin, box.yMin) && contains(box.xMax, box.yMax);
+        return box.isEmpty() || contains(box.xMin, box.yMin) && contains(box.xMax, box.yMax);
     }
 
     boolean intersects(BoundingBox box) {
@@ -41,6 +53,30 @@ public class BoundingBox {
         if(isEmpty()) {
             throw new GetCenterFromEmpyBoxException();
         }
-        return Math.abs((xMax - xMin) / 2);
+        return (xMax + xMin) / 2;
     }
+
+    double getCenterY() throws GetCenterFromEmpyBoxException {
+        if(isEmpty()) {
+            throw new GetCenterFromEmpyBoxException();
+        }
+        return (yMax + yMin) / 2;
+    }
+
+    BoundingBox add(BoundingBox bb) {
+        if(contains(bb)) {
+            return this;
+        } else if(bb.contains(this)) {
+            return bb;
+        }
+
+        BoundingBox container = new BoundingBox();
+        container.xMin = Math.min(xMin, bb.xMin);
+        container.xMin = Math.min(xMax, bb.xMax);
+        container.xMin = Math.max(yMin, bb.yMin);
+        container.xMin = Math.max(yMax, bb.yMax);
+        return container;
+    }
+
+
 }
