2b024539a138dd6d7a24c0297f9c95f607d40a0a
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-26 14:29:47 +0100

    Add BoundingBox fucntions

--- labki/src/main/java/com/gmail/konradkalita/lab7/BoundingBox.java
+++ labki/src/main/java/com/gmail/konradkalita/lab7/BoundingBox.java
@@ -1,5 +1,6 @@
 package com.gmail.konradkalita.lab7;
 
+import com.gmail.konradkalita.lab2_and_3.Matrix;
 import lombok.ToString;
 
 @ToString
@@ -8,4 +9,26 @@ public class BoundingBox {
     double ymin;
     double xmax;
     double ymax;
+    boolean isEmpty = false;
+
+    void addPoint(double x, double y) {
+
+    }
+
+    boolean contains(double x, double y){
+        return x >= xmin && x <= xmax && y >= ymin && y <= ymax;
+    }
+
+    boolean contains(BoundingBox box) {
+        return contains(box.xmin, box.ymin) && contains(box.xmax, box.ymax);
+    }
+
+    boolean intersects(BoundingBox box) {
+        return contains(box.xmin, box.ymin)
+                || contains(box.xmax, box.ymax)
+                || contains(box.xmin, box.ymax)
+                || contains(box.xmax, box.ymin);
+    }
+
+
 }
