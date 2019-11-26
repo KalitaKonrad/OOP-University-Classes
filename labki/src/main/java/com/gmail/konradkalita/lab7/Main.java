3b8bb49922492999597053d8104a61e8e0d6b4f1
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-26 10:30:11 +0100

    Change Main()

--- labki/src/main/java/com/gmail/konradkalita/lab7/Main.java
+++ labki/src/main/java/com/gmail/konradkalita/lab7/Main.java
@@ -1,10 +1,28 @@
 package com.gmail.konradkalita.lab7;
 
+import com.gmail.konradkalita.lab6.ColumnNotFoundException;
+import com.gmail.konradkalita.lab6.EmptyColumnValueException;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Main {
+
     public static void main(String[] args) {
-        AdminUnit adminUnit = new AdminUnit();
-        System.out.println(adminUnit.toString());
-        AdminUnitList unit = new AdminUnitList();
-        unit.read();
+        new Main();
+    }
+
+    public Main() {
+        AdminUnitList list = new AdminUnitList();
+        String filePath = getClass().getResource("/admin-units.csv").getPath();
+
+        try {
+            list.read(filePath);
+            list.list(System.out, 0, 1);
+            System.out.println(list.get(0).children);
+
+        } catch (IOException | EmptyColumnValueException | ColumnNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
