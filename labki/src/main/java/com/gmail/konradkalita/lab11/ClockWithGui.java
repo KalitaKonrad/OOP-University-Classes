c2ed070b26ad641360aa64e8eb903ad2dac0868e
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-12-17 15:28:30 +0100

    Change ClockWithGui class, add DownloadExample class

--- labki/src/main/java/com/gmail/konradkalita/lab11/ClockWithGui.java
+++ labki/src/main/java/com/gmail/konradkalita/lab11/ClockWithGui.java
@@ -28,34 +28,40 @@ public class ClockWithGui extends JPanel {
         Graphics2D g2d=(Graphics2D)g;
         g2d.translate(getWidth()/2,getHeight()/2);
 
-        for(int i = 1; i < 13; i++){
+        for(int i = 1; i < 13; i++) {
             AffineTransform at = new AffineTransform();
-            at.rotate(2 * Math.PI/12 * i);
-            Point2D src = new Point2D.Float(0,-120);
+            at.rotate(2 * Math.PI / 12 * i);
+            Point2D src = new Point2D.Float(0, -120);
             Point2D trg = new Point2D.Float();
-            at.transform(src,trg);
+            at.transform(src, trg);
 
             String text = Integer.toString(i);
-            int x = (int)(trg.getX() - g2d.getFontMetrics().stringWidth(text) / 2);
-            int y = (int)trg.getY();
+            int x = (int) (trg.getX() - g2d.getFontMetrics().stringWidth(text) / 2);
+            int y = (int) trg.getY();
 
-            g2d.drawString(Integer.toString(i), (int)trg.getX(), (int)trg.getY());
+            g2d.drawString(text, x, y);
 
             AffineTransform saveAT = g2d.getTransform();
-            g2d.rotate(time.getHour() % 12 * 2 * Math.PI/12);
-            g2d.drawLine(0,0,0,-100);
+            g2d.rotate(time.getHour() % 12 * 2 * Math.PI / 12);
+            g2d.drawLine(0, 0, 0, -100);
             g2d.setTransform(saveAT);
 
             AffineTransform saveAT2 = g2d.getTransform();
-            g2d.rotate(time.getMinute() * 2 * Math.PI/60);
-            g2d.drawLine(0,0,0,-100);
+            g2d.rotate(time.getMinute() * 2 * Math.PI / 60);
+            g2d.drawLine(0, 0, 0, -100);
             g2d.setTransform(saveAT2);
 
             AffineTransform saveAT3 = g2d.getTransform();
             System.out.println(time.getSecond());
-            g2d.rotate(time.getSecond() * 2 * Math.PI/60);
-            g2d.drawLine(0,0,0,-100);
+            g2d.rotate(time.getSecond() * 2 * Math.PI / 60);
+            g2d.drawLine(0, 0, 0, -100);
             g2d.setTransform(saveAT3);
+
+            // lines at hours ??????
+            AffineTransform saveAT4 = g2d.getTransform();
+            g2d.rotate(time.getHour() % 12 * 2 * Math.PI/12);
+            g2d.drawLine(100,100,0,-100);
+            g2d.setTransform(saveAT);
         }
 
     }
