e7f0f47d5a9e629105d532ee2355f42ef8470fb9
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-01-15 22:56:47 +0100

    Finish Mean class

--- labki/src/main/java/com/gmail/konradkalita/lab12/Mean.java
+++ labki/src/main/java/com/gmail/konradkalita/lab12/Mean.java
@@ -1,11 +1,16 @@
 package com.gmail.konradkalita.lab12;
 
+import lombok.SneakyThrows;
+
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Mean {
   static double[] array;
+  static BlockingQueue<Double> results = new ArrayBlockingQueue<>(100);
 
   static void initArray(int size) {
     array = new Random().doubles(size, 1, 100).toArray();
@@ -25,10 +30,11 @@ public class Mean {
       return Arrays.stream(array).average().getAsDouble();
     }
 
+    @SneakyThrows
     public void run() {
       double[] subArray = subArray(array, this.start, this.end);
       this.mean = computeAverage(subArray);
-      System.out.printf(Locale.US, "%d-%d mean=%f\n", start, end, mean);
+      results.put(this.mean);
     }
   }
 
@@ -36,8 +42,37 @@ public class Mean {
     return Arrays.copyOfRange(array, beg, end + 1);
   }
 
-  static void parallelMean(int threadCount) {
+  static void parallelMean(int threadCount) throws InterruptedException {
+    MeanCalc[] threads = new MeanCalc[threadCount];
+    int threadArraySize = array.length / threadCount;
 
+    for (int i = 0; i < threadCount; i++) {
+      threads[i] = new MeanCalc(i * threadArraySize, (i + 1) * threadArraySize);
+    }
+
+    double t1 = System.nanoTime() / 1e6;
+    Arrays.stream(threads).forEach(MeanCalc::start);
+    double t2 = System.nanoTime() / 1e6;
+
+    for (MeanCalc thread : threads) {
+      thread.join();
+    }
+
+    double t3 = System.nanoTime() / 1e6;
+    double finalMean =
+        Arrays.stream(threads).mapToDouble(thread -> thread.mean).average().getAsDouble();
+    System.out.printf(
+        Locale.US,
+        "PARALLEL MEAN -> size = %d threadCount=%d >  t2-t1=%f t3-t1=%f mean=%f\n",
+        array.length,
+        threadCount,
+        t2 - t1,
+        t3 - t1,
+        finalMean);
+  }
+
+  static void parrarelMeanV2(int threadCount) throws InterruptedException {
+    double sumOfMeans = 0;
     MeanCalc[] threads = new MeanCalc[threadCount];
     int threadArraySize = array.length / threadCount;
 
@@ -48,28 +83,24 @@ public class Mean {
     Arrays.stream(threads).forEach(MeanCalc::start);
     double t2 = System.nanoTime() / 1e6;
 
-    for (MeanCalc thread : threads) {
-      try {
-        thread.join();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+    for (int i = 0; i < threadCount; i++) {
+      sumOfMeans += results.take();
     }
-    double mean = Arrays.stream(threads).mapToDouble(thread -> thread.mean).average().getAsDouble();
+
     double t3 = System.nanoTime() / 1e6;
     System.out.printf(
         Locale.US,
-        "size = %d threadCount=%d >  t2-t1=%f t3-t1=%f mean=%f\n",
+        "PARALLEL MEANV2 -> size = %d threadCount=%d >  t2-t1=%f t3-t1=%f mean=%f\n",
         array.length,
         threadCount,
         t2 - t1,
         t3 - t1,
-        mean);
+        sumOfMeans / threadCount);
   }
 
-  public static void main(String[] args) {
-    initArray(100000000);
-    parallelMean(1000);
+  public static void main(String[] args) throws InterruptedException {
+    initArray(1000000);
     parallelMean(10);
+    parrarelMeanV2(10);
   }
 }
