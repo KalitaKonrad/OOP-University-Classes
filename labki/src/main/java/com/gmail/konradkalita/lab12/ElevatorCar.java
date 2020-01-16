264a8103b684d370250e56e90773686e846782b4
Konrad Kalita <konradkalita@student.agh.edu.pl>
2020-01-16 12:19:23 +0100

    Add ElevatorCar class + ElevatorStops class

---
+++ labki/src/main/java/com/gmail/konradkalita/lab12/ElevatorCar.java
@@ -0,0 +1,69 @@
+package com.gmail.konradkalita.lab12;
+
+public class ElevatorCar extends Thread {
+  int floor = 0;
+  Movement movementState = Movement.STOP;
+  Tour tour = Tour.UP;
+
+  public int getFloor() {
+    return floor;
+  }
+
+  enum Tour {
+    UP,
+    DOWN
+  };
+
+  enum Movement {
+    STOP,
+    MOVING
+  };
+
+  public void run() {
+    for (; ; ) {
+      // sleep(500);
+      if (movementState == Movement.STOP && tour == Tour.DOWN) {
+        if (!ElevatorStops.get().hasStopBelow(floor)) tour = Tour.UP;
+        else movementState = Movement.MOVING;
+        continue;
+      }
+      if (movementState == Movement.STOP && tour == Tour.UP) {
+        if (!ElevatorStops.get().hasStopAbove(floor)) tour = Tour.DOWN;
+        else movementState = Movement.MOVING;
+        continue;
+      }
+      if (movementState == Movement.MOVING && tour == Tour.DOWN) {
+        if (floor > ElevatorStops.get().getMinSetFloor()) {
+          floor--;
+          System.out.println("Floor" + floor);
+        } else {
+          movementState = Movement.STOP;
+          tour = Tour.UP;
+        }
+        if (ElevatorStops.get().whileMovingDownShouldStopAt(floor)
+            || floor == ElevatorStops.get().getMinSetFloor()) {
+          movementState = Movement.STOP;
+          ElevatorStops.get().clearStopDown(floor);
+          System.out.println("STOP");
+        }
+        continue;
+      }
+      if (movementState == Movement.MOVING && tour == Tour.UP) {
+        if (floor < ElevatorStops.get().getMaxSetFloor()) {
+          floor++;
+          System.out.println("Floor" + floor);
+        } else {
+          movementState = Movement.STOP;
+          tour = Tour.DOWN; // or Tour.UP? does it have meaning?
+        }
+        if (ElevatorStops.get().whileMovingUpShouldStopAt(floor)
+            || floor == ElevatorStops.get().getMaxSetFloor()) {
+          movementState = Movement.STOP;
+          ElevatorStops.get().clearStopUp(floor);
+          System.out.println("STOP");
+        }
+        continue;
+      }
+    }
+  }
+}
